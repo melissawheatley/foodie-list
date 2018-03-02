1ed45ef40f99738bc68ab5d5b588b6fb94fb378e
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-03-02 15:18:58 -0600

    dynamically populated select element by XHR request for cities

--- js/foodie-list.js
+++ js/foodie-list.js
@@ -9,15 +9,33 @@ function getRestaurants(){
     loader.addEventListener('load', function(){
         var allRestaurants = JSON.parse(this.responseText);
         resolve(allRestaurants);
-        console.log(allRestaurants);
+        // console.log(allRestaurants);
     });
     loader.addEventListener('error', function(){
-        reject(console.log("There was a problem loading restaurants."));
+        reject(console.log("There was a problem getting restaurants."));
     });
     loader.open("GET", `https://foodie-list-5b7c9.firebaseio.com/restaurants.json`);
     loader.send();
     });
 }
 
+function getCities(){
+    // console.log("getting all restaurants");
+    return new Promise((resolve,reject) => {
+        var loader = new XMLHttpRequest();
+    loader.addEventListener('load', function(){
+        var allCities = JSON.parse(this.responseText);
+        resolve(allCities);
+        console.log(allCities);
+    });
+    loader.addEventListener('error', function(){
+        reject(console.log("There was a problem getting cities."));
+    });
+    loader.open("GET", `https://foodie-list-5b7c9.firebaseio.com/cities.json`);
+    loader.send();
+    });
+}
+
+
 // getRestaurants();
-module.exports = {getRestaurants};
+module.exports = {getRestaurants, getCities};
